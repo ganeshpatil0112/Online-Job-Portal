dfeaed2b167d34a962594dae11396b80d03e518d
Ganesh Patil <62154770+ganeshpatil0112@users.noreply.github.com>
2022-08-02 16:11:20 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,31 @@
+
+
+<footer id="contactus" style="background-color: #272727;">
+    <div class="container" style="width: 100%;">
+				<div class="row justify-content-center">
+					<div class="col text-center">
+                        <img src="img/ftrlg.png" style="width:60px;" alt="">
+                        <h2 class="footer-heading"> 
+                        <a href="index.php" style="color: #ffffff; font-family: 'Sora', sans-serif; font-size: 50px; text-decoration: none;"> DreamJobs4U.in </a> 
+                        </h2>
+                    </div>	
+                                                
+                    <div class="menu text-center">
+                    <p>
+                    <a href="index.php" style="color: #ffd6a5; padding-top: 50px; padding-right: 40px; font-family:Comfortaa;"> HOME </a> 
+                    <a href="#jbs" style="color: #ffd6a5; padding-top: 50px; padding-right: 40px; font-family:Comfortaa;"> JOBS </a> 
+                    <a href="" style="color: #ffd6a5; padding-top: 50px; font-family:Comfortaa;"> CONTACT </a>
+                    </p>
+                    </div>
+					
+				</div>
+				
+                <div class="row mt-5">
+					<div class="col-md-12 text-center">
+						<p class="copyrigt">
+                        <a style="color: #9FA4A9; text-decoration: none;"> @2022 Project Done by Batch 121 | Group G </a> 
+					    </p>
+					</div>
+				</div>
+    </div>
+</footer>
